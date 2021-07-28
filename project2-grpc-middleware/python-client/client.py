9cd6572833b235167cd98d10acce3980fcbd7228
Gabriel Cipriano <gabrielfcipriano@gmail.com>
2021-07-27 23:39:51 -0300

    Agora vai

--- project2-grpc-middleware/python-client/client.py
+++ project2-grpc-middleware/python-client/client.py
@@ -5,38 +5,23 @@ from statistics import mean, median, pstdev
 import hashtable_pb2
 import hashtable_pb2_grpc
 
-from multiprocessing import Pool
+from multiprocessing import Pool, Process
 
-async def runGrpc(key: str, value: int) -> None:
-    async with grpc.aio.insecure_channel('localhost:50051') as channel:
-        stub = hashtable_pb2_grpc.HashtableStub(channel)
-        await stub.put(hashtable_pb2.putRequest(key=key, value=value))
-        response = await stub.get(hashtable_pb2.getRequest(key=key))
-        if(response.value != value):
-            print("ERRO: Valor do servidor não era o esperado", response.value, value)
-
-def runGrpcSync(par) -> None:
-    key, value = par
+def runGrpcSyncList(keys, values) -> None:
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = hashtable_pb2_grpc.HashtableStub(channel)
-        stub.put(hashtable_pb2.putRequest(key=key, value=value))
-        response = stub.get(hashtable_pb2.getRequest(key=key))
-        if(response.value != value):
-            print("ERRO: Valor do servidor não era o esperado", response.value, value)
-
-
-
-def runGrpc_sync(par) -> None:
-    key, value = par
-    asyncio.run(runGrpc(key, value))
-    return value
+        for key, value in zip(keys, values):
+            stub.put(hashtable_pb2.putRequest(key=key, value=value))
+            response = stub.get(hashtable_pb2.getRequest(key=key))
+            if(response.value != value):
+                print("ERRO: Valor do servidor não era o esperado", response.value, value)
 
 if __name__ == '__main__':
 
     tempo = []
 
     m = 10000
-    processos = 1
+    n_process = 4
     keys = []
     values = []
 
@@ -45,17 +30,22 @@ if __name__ == '__main__':
         keys.append(str(i))
         values.append(i)
 
-    p = Pool(processes=processos)
-    for i in range(5):
-        start = time()
-        # result = p.map(runGrpc_sync, zip(keys, values))
-        result = p.map(runGrpcSync, zip(keys, values))
-        tempo.append(time() - start)
-        print(f"Tempo({i}): ", tempo[i])
-    p.close()
-    p.join()
+    sz = m//n_process
 
-    print(f"\n{processos} processos:")
+    processos = [None] * n_process
+    for t in range(5):
+        start = time()
+        for i in range(n_process):
+            processos[i] = Process(target=runGrpcSyncList, args=(keys[i*sz:(i+1)*sz-1], values[i*sz:(i+1)*sz-1]))
+            processos[i].start()
+        
+        for p in processos:
+            p.join()
+
+        tempo.append(time() - start)
+        print(f"Tempo({t}): ", tempo[t])
+
+    print(f"\n{n_process} processos:")
     print(f"Média: {mean(tempo)}")
     print(f"Mediana: {median(tempo)}")
     print(f"Desvio Padrão: {pstdev(tempo)}")
