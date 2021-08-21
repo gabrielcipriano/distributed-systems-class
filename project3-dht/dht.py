5554d0145863132a5ef22b1cb067ec1618bda627
Athus Cavalini <athuscavalini@MacBook-Pro-de-Athus.local>
2021-08-20 22:01:01 -0300

    project 3 init

---
+++ project3-dht/dht.py
@@ -0,0 +1,29 @@
+import pika, sys, os, json
+from random import randrange
+from node import Node
+
+def main():
+
+    # Entra no broker
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    # Limpa a fila de mensagens
+    channel.queue_delete(queue='dht')
+
+    # Cria uma nova fila de mensagens
+    channel.queue_declare(queue='dht', durable=True)
+
+    # Inicializa os nós
+    # Falta colocar cada um em uma thread ou processo
+    nodes = [ Node() for _ in range(8) ] 
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
